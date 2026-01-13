343724c43010bfca9d147302d63c66c6f33a014d
Gobi Mohanathas <gobi.mohanathas7@gmail.com>
2026-01-13 17:06:27 -0500

    chore: loads database information from environment variable
    - Database information is now retrieved from .env file
    
    Signed-Off-By: Gobisangar Mohanathas

---
+++ backend/config/db.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Author: Gobi Mohanathas
+ * File Name: db.php
+ * Date of Creation: January 13, 2026
+ * Purpose: Database configuration file allowing connection to StreamingDB database on
+ *          PostgreSQL via PDO.
+ */
+
+// Check to see if .env file exists and prepares database information
+if (file_exists(__DIR__ . '/../.env')) {
+    foreach (file(__DIR__ . '/../.env') as $line) {
+        if (strpos(trim($line), '#') === 0 || !str_contains($line, '=')){
+            continue;
+        }
+        putenv(trim($line));
+    }
+}
+// Database host
+$host = getenv('DB_HOST');
+// Database port
+$port = getenv('DB_PORT');
+// Database name
+$dbname = getenv('DB_NAME');
+// Database user
+$user = getenv('DB_USER');
+// Database password
+$password = getenv('DB_PASSWORD');
+
+// Storing information for which database, where and what name
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
+
+// Attempt to create PDO object using above information and setting the error attribute to throw
+// exceptions, if unsuccessful then stop script and display error message.
+try {
+    $db = new PDO($dsn, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+}
+catch (PDOException $e) {
+    http_response_code(500);
+    exit("Database connection failed");
+}
